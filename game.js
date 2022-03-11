27bd8c2684cc3accaac744cbeb86c6028d36dcdc
Shoenemann <85433055+Shoenemann@users.noreply.github.com>
2022-03-11 20:10:20 +0100

    Create function roundGame

--- game.js
+++ game.js
@@ -5,12 +5,51 @@ function computerPlay() {
     return mani[i]
 }
 
-function messageWin (playerSelection, computerSelection) {
-    return `You Win! ${playerSelection} beats ${computerSelection}.` 
+function itWins(i,j) {
+    if (i == ((j+1)%3)) {
+        return true
+    } 
+    else {
+        return false
+    }
 }
 
-function messageLose (playerSelection,computerSelection) {
-    return `You Lose! ${computerSelection} beats ${playerSelection}.` 
+function roundGame(playerSelection,computerSelection) {
+    let p = formatSelection(playerSelection)
+    let c = formatSelection(computerSelection)
+    if(p == c) {
+        return messageDraw(p)
+    }
+    let i = mani.indexOf(p)
+    let j = mani.indexOf(c)
+    if ( itWins(i,j) ) {
+        return messageWin(p,c)
+    }
+    else {
+        return messageLose(c,p)
+    }
 }
+
+function formatSelection(wOrD) {
+    word=wOrD.toLowerCase();
+    let firstLetter=word.substr(0,1)
+    let otherLetters = word.substr(1)
+    let F=firstLetter.toUpperCase()
+    return F + otherLetters
+}
+
+function messageDraw(drawSelection) {
+    return `It's a Draw! Both of you chose ${drawSelection}.`
+}
+
+function messageWin (winSelection, loseSelection) {
+    return `You Win! ${winSelection} beats ${loseSelection}.` 
+}
+
+function messageLose (winSelection,loseSelection) {
+    return `You Lose! ${winSelection} beats ${loseSelection}.` 
+}
+
+
 
  
