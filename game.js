71f5b4c11fe0cc0c40a2a2f154fc35813991f361
Shoenemann <85433055+Shoenemann@users.noreply.github.com>
2022-03-11 19:40:09 +0100

    Create messages Lose, Win

--- game.js
+++ game.js
@@ -5,3 +5,12 @@ function computerPlay() {
     return mani[i]
 }
 
+function messageWin (playerSelection, computerSelection) {
+    return `You Win! ${playerSelection} beats ${computerSelection}.` 
+}
+
+function messageLose (playerSelection,computerSelection) {
+    return `You Lose! ${computerSelection} beats ${playerSelection}.` 
+}
+
+ 
